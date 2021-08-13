9ae89722ebfc3c20517ac1f386805e399876032a
Felipe-BP <gvv.fel@gmail.com>
2021-08-12 22:36:37 -0300

    refactor: setup store logger middleware only in dev mode
    - Avoid logging unnecessary data on console and module hot reload in prod envs

--- src/store/store.ts
+++ src/store/store.ts
@@ -2,21 +2,24 @@ import { configureStore, ThunkAction, Action, combineReducers } from '@reduxjs/t
 
 import charactersReducer from '../features/characters/characterSlice';
 import loggerMiddleware from '../middlewares/logger';
+import { isDevMode } from '../utils/isDevMode';
 
 export default function configureAppStore() {
     const store = configureStore({
       reducer: {
         characters: charactersReducer,
       },
-      middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(loggerMiddleware)
+      middleware: (getDefaultMiddleware) => isDevMode()
+        ? getDefaultMiddleware().concat(loggerMiddleware)
+        : getDefaultMiddleware()
     });
 
-    if (process.env.NODE_ENV !== 'production' && module.hot) {
-        module.hot.accept('../features/characters/characterSlice', () =>
-            store.replaceReducer(combineReducers({
-                characters: charactersReducer,
-            }))
-        )
+    if (isDevMode() && module.hot) {
+      module.hot.accept('../features/characters/characterSlice', () =>
+        store.replaceReducer(combineReducers({
+            characters: charactersReducer,
+        }))
+      )
     }
 
     return store;
