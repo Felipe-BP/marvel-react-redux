28927c5e08f5386f08afed24709e66960049d42f
Felipe-BP <gvv.fel@gmail.com>
2021-08-14 00:50:25 -0300

    style: refactor blank container content on app component

---
+++ src/pages/CharacterDetail.tsx
@@ -0,0 +1,12 @@
+import { useEffect } from 'react';
+// import { useLocation } from 'react-router-dom';
+
+export function CharacterDetail() {
+    // const { state } = useLocation();
+
+    useEffect(() => {}, []);
+
+    return (
+        <div>Character</div>
+    );
+}
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,7 +1,7 @@
 import { useEffect } from "react";
 import { useDispatch } from "react-redux";
-import { Row, Col, Layout, Pagination, Empty, Spin } from "antd";
-import { LoadingOutlined } from '@ant-design/icons';
+import { Row, Col, Pagination, Empty, Spin } from "antd";
+import { LoadingOutlined } from "@ant-design/icons";
 
 import "../styles/home.css";
 
@@ -13,8 +13,6 @@ import {
 } from "../features/characters/characterSlice";
 import { CharacterCard } from "../components/CharacterCard";
 import { CharacterSearch } from "../components/CharacterSearch";
-
-const { Content } = Layout;
 
 const antIcon = <LoadingOutlined style={{ fontSize: 24 }} spin />;
 
@@ -33,40 +31,37 @@ export function Home() {
   }, [dispatch, currentPage, searchValue]);
 
   return (
-    <Content className="content-container">
-      <div className="layout-content">
-        <Row className="search" justify="center">
-          <Col span={10}>
-            <CharacterSearch />
-          </Col>
-        </Row>
+    <>
+      <Row className="search" justify="center">
+        <Col span={10}>
+          <CharacterSearch />
+        </Col>
+      </Row>
 
-        {status === Status.LOADING && <Spin indicator={antIcon} />}
+      {status === Status.LOADING && <Spin indicator={antIcon} />}
 
-        {status === Status.IDLE && !characters.length && (<Empty />)}
+      {status === Status.IDLE && !characters.length && <Empty />}
 
-        {status === Status.IDLE && characters.length && (
-          <>
-            <Row gutter={{ xs: 8, sm: 16, md: 24, lg: 32 }}>
-              {characters?.map((character) => (
-                <Col className="column" key={character.id}>
-                  <CharacterCard character={character} />
-                </Col>
-              ))}
-            </Row>
-            <Row justify="center">
-              <Pagination
-                current={currentPage + 1}
-                total={total}
-                pageSize={limit}
-                pageSizeOptions={[]}
-                onChange={(page) => dispatch(changeCurrentPage(page - 1))}
-              />
-            </Row>
-          </>
-        )}
-
-      </div>
-    </Content>
+      {status === Status.IDLE && characters.length && (
+        <>
+          <Row gutter={{ xs: 8, sm: 16, md: 24, lg: 32 }}>
+            {characters?.map((character) => (
+              <Col className="column" key={character.id}>
+                <CharacterCard character={character} />
+              </Col>
+            ))}
+          </Row>
+          <Row justify="center">
+            <Pagination
+              current={currentPage + 1}
+              total={total}
+              pageSize={limit}
+              pageSizeOptions={[]}
+              onChange={(page) => dispatch(changeCurrentPage(page - 1))}
+            />
+          </Row>
+        </>
+      )}
+    </>
   );
 }
