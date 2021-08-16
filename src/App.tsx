bb93fc729de8fc772dd884f222b5377938d87387
Felipe-BP <gvv.fel@gmail.com>
2021-08-16 01:55:41 -0300

    fix: unexpected error because exists a div inside Switch component

--- src/App.tsx
+++ src/App.tsx
@@ -18,19 +18,19 @@ function App() {
         </a>
       </Header>
 
-      <BrowserRouter>
-        <Switch>
-          <Content className="content-container">
-            <div className="layout-content">
-              <Route exact path="/" component={Home} />
-              <Route
-                path="/character/:characterId/details"
-                component={CharacterDetail}
-              />
-            </div>
-          </Content>
-        </Switch>
-      </BrowserRouter>
+    <Content className="content-container">
+      <div className="layout-content">
+        <BrowserRouter>
+          <Switch>
+            <Route exact path="/" component={Home} />
+            <Route
+              path="/character/:characterId/details"
+              component={CharacterDetail}
+            />
+          </Switch>
+        </BrowserRouter>
+      </div>
+    </Content>
 
       <Footer className="footer">Made with 💗 by Felipe Bueno!</Footer>
     </Layout>
